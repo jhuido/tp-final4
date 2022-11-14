779d147c58c4cba03d65ea4080ee26e57c1f2ae0
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-13 22:01:48 -0300

    'TDDiteration'

--- src/Local.js
+++ src/Local.js
@@ -2,9 +2,8 @@ function Local(nombre,fila){
     this.nombre=nombre;
     this.fila=fila;
 
-    this.generePaquete=function(mapa,destino){
+    this.generePaquete=function(destino){
         const paquete=new Paquete(destino);
-        mapa[fila][0].push(paquete);
         return paquete;
     }
 }
--- test/paquete.test.js
+++ test/paquete.test.js
@@ -5,7 +5,7 @@ const Paquete = require("../src/Paquete");
 test("Crear Paquete",()=>{
     const local=new Local("A");
     const destino=new Destino(1);
-    const paquete=local.generePaquete();
+    const paquete=local.generePaquete(destino);
     expect(paquete.tiempo).toBe(0);
     expect(paquete.destino.nombre).toBe(1);
 })
