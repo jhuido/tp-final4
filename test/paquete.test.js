4aedc1e31e9de0762e787d205f897fa9b9312281
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-13 23:49:54 -0300

    'TDDiteration'

--- test/paquete.test.js
+++ test/paquete.test.js
@@ -3,9 +3,15 @@ const Local = require("../src/Local");
 const Paquete = require("../src/Paquete");
 
 test("Crear Paquete",()=>{
-    const local=new Local("A");
-    const destino=new Destino(1);
-    const paquete=local.generePaquete(destino);
+    const facturacion=new Centro("Facturacion",3);
+    const calidad=new Centro("Calidad",4);
+    const distribucion=new Centro("Distribucion",5);
+    const centros=[[facturacion,2],[calidad,5],[distribucion,8]];
+    const local=new Local("A",0,centros);
+    const destino=new Destino(1,0);
+    const locales=[local];
+    const mapa=new Mapa(locales.length,centros.length);
+    const paquete=local.generePaquete(destino,mapa);
     expect(paquete.tiempo).toBe(0);
     expect(paquete.destino.nombre).toBe(1);
 })
