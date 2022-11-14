e71ba7935321b35b645f48ee09c5c9d70e04ee3b
jhuido <josefinahuidobro@uca.edu.ar>
2022-11-13 21:36:44 -0300

    'TDDiteration'

---
+++ src/Local.js
@@ -0,0 +1,6 @@
+function Local(nombre){
+    this.nombre=nombre;
+
+}
+
+module.exports=Local;
--- test/local.test.js
+++ test/local.test.js
@@ -1,3 +1,5 @@
+const Local=require("../src/Local");
+
 test("Crear Local",()=>{
     const local=new Local();
     expect(local.nombre).toBe("A");
